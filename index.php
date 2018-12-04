cb0ffb55c6b6c4402dd4d7746320b09b1c526a8e
Adam <flashbackditto@users.noreply.github.com>
2018-12-04 02:03:15 -0800

    Made arriving emails better formatted via PHP mail

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+if (isset($_POST['submit'])) {
+  $name = $_POST['name'];
+  $subject = $_POST['subject'];
+  $mailFrom = $_POST['email'];
+  $phone = $_POST['phone'];
+  $message = $_POST['comments'];
+  $subscribe = $_POST['subscribe'];
+
+$mailTo = "chaunceyberkowits@yahoo.com"; //cant sent to gmail
+$headers = "From: $mailFrom";
+$txt = "You have received an email from $name.\n\n $message\n\n Phone number: $phone";
+
+  mail($mailTo, $subject, $txt, $headers, $subscribe );
+  header("Location: index.php?mailsend");
+  echo "Thank You!";
+};
+?>
